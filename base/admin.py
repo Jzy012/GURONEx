c56b774f2b4fd687411845b896ce05cbb5a980c9
Jesse Carl Nodalo <jessecarlnodalo@iskolarngbayan.pup.edu.ph>
2025-05-04 22:54:21 +0800

    Added the Custom User Model to the System Admin Panel

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,85 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from django.contrib.auth.admin import UserAdmin
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.utils.translation import gettext_lazy as _
+from .models import Account  # your custom user model
+
+
+class AccountCreationForm(forms.ModelForm):
+    """Form for creating new users (in admin add user page)"""
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = Account
+        fields = ('email', 'role')
+
+    def clean_password2(self):
+        # Check if passwords match
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        # Save hashed password
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+
+        # Automatically set is_staff/is_superuser if role == system_admin
+        if user.role == 'system_admin':
+            user.is_staff = True
+            user.is_superuser = True
+        else:
+            user.is_staff = False
+            user.is_superuser = False
+
+        if commit:
+            user.save()
+        return user
+
+class AccountChangeForm(forms.ModelForm):
+    """Form for updating existing users in admin"""
+    password = forms.CharField(label='Password', widget=forms.PasswordInput, required=False)
+
+    class Meta:
+        model = Account
+        fields = ('email', 'role', 'password', 'is_active', 'is_staff', 'is_superuser')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if self.cleaned_data['password']:
+            user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
+
+class AccountAdmin(UserAdmin):
+    """Custom admin class to control admin UI for Accounts"""
+    add_form = AccountCreationForm
+    form = AccountChangeForm
+    model = Account
+
+    list_display = ('email', 'role', 'is_staff', 'is_superuser')
+    list_filter = ('role', 'is_staff', 'is_superuser')
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password', 'role')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'role', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('email',)
+    ordering = ('email',)
+
+admin.site.register(Account, AccountAdmin)
