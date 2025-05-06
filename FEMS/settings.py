cd222872f4ffa216024f4e3cee713e4c30822165
Jesse Carl Nodalo <jessecarlnodalo@iskolarngbayan.pup.edu.ph>
2025-05-06 22:54:00 +0800

    Configure URLS

--- FEMS/settings.py
+++ FEMS/settings.py
@@ -40,6 +40,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'base.apps.BaseConfig',
+    'adminhub.apps.AdminhubConfig',
+    'faculty.apps.FacultyConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
