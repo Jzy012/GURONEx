edb4f2f1ddacf99540d8756e089e0d7d2e0ef4ae
Jesse Carl Nodalo <jessecarlnodalo@iskolarngbayan.pup.edu.ph>
2025-12-04 22:54:16 +0800

    fix:ssl error fixed

--- FEMS/settings.py
+++ FEMS/settings.py
@@ -37,6 +37,7 @@ DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=["http://localhost:8000"])
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 SECURE_SSL_REDIRECT = env.bool("SECURE_SSL_REDIRECT", default=False)
 
 # Only send cookies over HTTPS
